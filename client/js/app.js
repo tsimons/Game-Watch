e8ab0417b0b4882f4f7325a9f630dc9ac05f395c
TJ <t.simons88@gmail.com>
2015-02-19 21:43:06 -0600

    added material ui framework. switched from less to sass. added react-router

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,26 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react')
+  , Router = require('react-router')
+  , RouteHandler = Router.RouteHandler
+;
 
 var App = React.createClass({
+  mixins: [Router.State],
   getDefaultProps: function () {
     return {
       greeting: "Hello"
     } 
   },
   render: function() {
+    console.log('Params: %O', this.getParams());
+    
     return (
-      <h1>{this.props.greeting} World!</h1>
+      <div>
+        <h1>{this.props.greeting} {this.getParams().person}!</h1>
+
+        <RouteHandler />
+      </div>
     );
   }
 
